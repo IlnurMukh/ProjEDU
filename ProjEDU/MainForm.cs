2709910c03aee79ae7d89042894db7b7fb78a42e
IlnurMukh <ilnur3793@gmail.com>
2024-02-27 23:29:48 +0300

    Добавил int c

--- ProjEDU/MainForm.cs
+++ ProjEDU/MainForm.cs
@@ -20,7 +20,7 @@ namespace ProjEDU
 
         private void theoryToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            int c;
         }
 
         private void testToolStripMenuItem_Click(object sender, EventArgs e)
