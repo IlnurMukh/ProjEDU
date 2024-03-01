828d5190cadd12a635d0d930cddeee26e799b1a7
IlnurMukh <ilnur3793@gmail.com>
2024-03-01 21:48:18 +0300

    Добавил штучки в EditForm

--- ProjEDU/EditForm.Designer.cs
+++ ProjEDU/EditForm.Designer.cs
@@ -28,65 +28,179 @@
         /// </summary>
         private void InitializeComponent()
         {
-            panel1 = new Panel();
-            label1 = new Label();
-            comboBox1 = new ComboBox();
-            panel2 = new Panel();
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            richTextBox1 = new RichTextBox();
+            treeView1 = new TreeView();
+            tabPage2 = new TabPage();
+            tabPage3 = new TabPage();
+            menuStrip1 = new MenuStrip();
+            exitToolStripMenuItem = new ToolStripMenuItem();
+            editToolStripMenuItem = new ToolStripMenuItem();
+            richTextBox2 = new RichTextBox();
+            treeView2 = new TreeView();
+            richTextBox3 = new RichTextBox();
+            treeView3 = new TreeView();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            tabPage2.SuspendLayout();
+            tabPage3.SuspendLayout();
+            menuStrip1.SuspendLayout();
             SuspendLayout();
             // 
-            // panel1
+            // tabControl1
             // 
-            panel1.Location = new Point(301, 12);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(965, 480);
-            panel1.TabIndex = 0;
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Controls.Add(tabPage3);
+            tabControl1.Location = new Point(3, 33);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(1272, 459);
+            tabControl1.TabIndex = 4;
             // 
-            // label1
+            // tabPage1
             // 
-            label1.AutoSize = true;
-            label1.Location = new Point(12, 9);
-            label1.Name = "label1";
-            label1.Size = new Size(277, 20);
-            label1.TabIndex = 1;
-            label1.Text = "Выберите объект для редактирования";
+            tabPage1.Controls.Add(richTextBox1);
+            tabPage1.Controls.Add(treeView1);
+            tabPage1.Location = new Point(4, 29);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(1264, 426);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "Текст";
+            tabPage1.UseVisualStyleBackColor = true;
             // 
-            // comboBox1
+            // richTextBox1
             // 
-            comboBox1.FormattingEnabled = true;
-            comboBox1.Items.AddRange(new object[] { "Текст", "Картинки", "Видео", "Тест" });
-            comboBox1.Location = new Point(12, 32);
-            comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(151, 28);
-            comboBox1.TabIndex = 2;
-            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            richTextBox1.Location = new Point(218, 6);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(1040, 414);
+            richTextBox1.TabIndex = 1;
+            richTextBox1.Text = "\n";
             // 
-            // panel2
+            // treeView1
             // 
-            panel2.Location = new Point(12, 66);
-            panel2.Name = "panel2";
-            panel2.Size = new Size(277, 426);
-            panel2.TabIndex = 3;
+            treeView1.Location = new Point(6, 6);
+            treeView1.Name = "treeView1";
+            treeView1.RightToLeft = RightToLeft.No;
+            treeView1.Size = new Size(206, 346);
+            treeView1.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            tabPage2.AutoScroll = true;
+            tabPage2.BackgroundImageLayout = ImageLayout.None;
+            tabPage2.Controls.Add(richTextBox2);
+            tabPage2.Controls.Add(treeView2);
+            tabPage2.Location = new Point(4, 29);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(1264, 426);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "Картинки";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            tabPage3.Controls.Add(richTextBox3);
+            tabPage3.Controls.Add(treeView3);
+            tabPage3.Location = new Point(4, 29);
+            tabPage3.Name = "tabPage3";
+            tabPage3.Size = new Size(1264, 426);
+            tabPage3.TabIndex = 2;
+            tabPage3.Text = "Видео";
+            tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { exitToolStripMenuItem, editToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(1278, 28);
+            menuStrip1.TabIndex = 5;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // exitToolStripMenuItem
+            // 
+            exitToolStripMenuItem.Alignment = ToolStripItemAlignment.Right;
+            exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            exitToolStripMenuItem.Size = new Size(67, 24);
+            exitToolStripMenuItem.Text = "Выход";
+            // 
+            // editToolStripMenuItem
+            // 
+            editToolStripMenuItem.Name = "editToolStripMenuItem";
+            editToolStripMenuItem.Size = new Size(97, 24);
+            editToolStripMenuItem.Text = "Сохранить";
+            // 
+            // richTextBox2
+            // 
+            richTextBox2.Location = new Point(218, 6);
+            richTextBox2.Name = "richTextBox2";
+            richTextBox2.Size = new Size(1040, 414);
+            richTextBox2.TabIndex = 3;
+            richTextBox2.Text = "\n";
+            // 
+            // treeView2
+            // 
+            treeView2.Location = new Point(6, 6);
+            treeView2.Name = "treeView2";
+            treeView2.RightToLeft = RightToLeft.No;
+            treeView2.Size = new Size(206, 346);
+            treeView2.TabIndex = 2;
+            // 
+            // richTextBox3
+            // 
+            richTextBox3.Location = new Point(218, 6);
+            richTextBox3.Name = "richTextBox3";
+            richTextBox3.Size = new Size(1040, 414);
+            richTextBox3.TabIndex = 3;
+            richTextBox3.Text = "\n";
+            // 
+            // treeView3
+            // 
+            treeView3.Location = new Point(6, 6);
+            treeView3.Name = "treeView3";
+            treeView3.RightToLeft = RightToLeft.No;
+            treeView3.Size = new Size(206, 346);
+            treeView3.TabIndex = 2;
             // 
             // EditForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1278, 524);
-            Controls.Add(panel2);
-            Controls.Add(comboBox1);
-            Controls.Add(label1);
-            Controls.Add(panel1);
+            Controls.Add(menuStrip1);
+            Controls.Add(tabControl1);
             Name = "EditForm";
             Text = "EditForm";
+            Load += EditForm_Load;
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage2.ResumeLayout(false);
+            tabPage3.ResumeLayout(false);
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private Panel panel1;
-        private Label label1;
-        private ComboBox comboBox1;
-        private Panel panel2;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private RichTextBox richTextBox1;
+        private TreeView treeView1;
+        private TabPage tabPage2;
+        private TabPage tabPage3;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem exitToolStripMenuItem;
+        private ToolStripMenuItem editToolStripMenuItem;
+        private RichTextBox richTextBox2;
+        private TreeView treeView2;
+        private RichTextBox richTextBox3;
+        private TreeView treeView3;
     }
 }
--- ProjEDU/MainForm.cs
+++ ProjEDU/MainForm.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.XPath;
 
 namespace ProjEDU
 {
@@ -15,13 +18,13 @@ namespace ProjEDU
         public MainForm(string name, bool isTeacher)
         {
             InitializeComponent();
-            if(!isTeacher)
-            {   
+            if (!isTeacher)
+            {
                 editToolStripMenuItem.Enabled = false;
                 editToolStripMenuItem.Visible = false;
             }
             Text = $"Вы вошли под логином {name}";
-            
+
 
         }
 
@@ -32,16 +35,30 @@ namespace ProjEDU
 
         private void editToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            EditForm editForm = new EditForm();
+            Visible = false;
         }
-        private void panel1_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
+        
 
         private void MainForm_Load(object sender, EventArgs e)
         {
-
+            XmlDocument doc = new XmlDocument();
+            doc.Load("C:\\Users\\Ilnur\\source\\repos\\ProjEDU\\ProjEDU\\XMLFiles\\TextTree.xml");
+            //doc.Load("C:\\Users\\Vostro\\source\\repos\\ProjEDU\\ProjEDU\\XMLFiles\\TextTree.xml");
+            foreach (XmlNode node in doc.SelectNodes("//node"))
+            {
+                var treeNode = new TreeNode();
+                treeNode.Text = node.Attributes["text"].Value;
+                treeNode.Name = node.Attributes["name"].Value;
+                foreach(XmlNode child in node.ChildNodes)
+                {
+                    var childNode = new TreeNode();
+                    childNode.Text = child.Attributes["text"].Value;
+                    childNode.Name = child.Attributes["name"].Value;
+                    treeNode.Nodes.Add(childNode);
+                }
+                treeView1.Nodes.Add(treeNode);
+            }
         }
 
         private void btnTheory_Click(object sender, EventArgs e)
@@ -50,19 +67,65 @@ namespace ProjEDU
 
         }
 
-        private void panel1_Paint_1(object sender, PaintEventArgs e)
-        {
-
-        }
+       
 
         private void btnForward_Click(object sender, EventArgs e)
         {
+            //XmlDocument doc = new XmlDocument();
+            //XmlElement root = doc.CreateElement("tree");
+            //doc.AppendChild(root);
+
+            //for (int i = 0; i < treeView1.Nodes.Count; i++)
+            //{
+            //    XmlElement element = doc.CreateElement("node");
+            //    element.SetAttribute("text", treeView1.Nodes[i].Text);
+            //    element.SetAttribute("name", treeView1.Nodes[i].Name);
+            //    root.AppendChild(element);
+            //    for (int j = 0; j < treeView1.Nodes[i].Nodes.Count; j++)
+            //    {
+            //        XmlElement childElement = doc.CreateElement("node");
+            //        childElement.SetAttribute("text", treeView1.Nodes[i].Nodes[j].Text);
+            //        childElement.SetAttribute("name", treeView1.Nodes[i].Nodes[j].Name);
+            //        element.AppendChild(childElement);
+            //    }
+            //}
+
+            //doc.Save("C:\\Users\\Vostro\\source\\repos\\ProjEDU\\ProjEDU\\XMLFiles\\TextTree.xml");
+
+            //string filePath = "C:\\Users\\Vostro\\source\\repos\\ProjEDU\\ProjEDU\\Images\\Image1.png";
+
+            //XmlDocument xmlDocument = new XmlDocument();
+
+            //XmlElement rootElement = xmlDocument.CreateElement("image");
+            //xmlDocument.AppendChild(rootElement);
+
+            //XmlElement imageElement = xmlDocument.CreateElement("data");
+            //imageElement.SetAttribute("type", "image/png");
+            //imageElement.InnerText = File.ReadAllText(filePath);
+            //rootElement.AppendChild(imageElement);
+
+            //xmlDocument.Save("C:\\Users\\Vostro\\source\\repos\\ProjEDU\\ProjEDU\\XMLFiles\\Image.xml");
+
+
+
+            XDocument doc = XDocument.Load("C:\\Users\\Vostro\\source\\repos\\ProjEDU\\ProjEDU\\XMLFiles\\Image.xml");
+            var imageElement = doc.XPathSelectElement("//image/data");
+            if (imageElement != null)
+            {
+                byte[] imageData = Convert.FromBase64String(imageElement.Value);
+                using (var ms = new MemoryStream(imageData, 0, imageData.Length))
+                {
+                    ms.Write(imageData, 0, imageData.Length);
+                    var bitmap = (Bitmap)Image.FromStream(ms);
+                    bitmap.Save("C:\\Users\\Vostro\\source\\repos\\ProjEDU\\ProjEDU\\Images\\NewImage.jpg");
+                }
+            }
 
         }
 
         private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
         {
-            
+
             richTextBox1.Text = TakeText($"{e.Node.Name}.txt");
         }
         private string TakeText(string FileName)
@@ -78,5 +141,9 @@ namespace ProjEDU
             }
             return "";
         }
+
+       
+
+        
     }
 }
