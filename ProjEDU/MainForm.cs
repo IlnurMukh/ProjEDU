a3ade31c4f27bda09fac3ba7834397ea775b8cec
IlnurMukh <ilnur3793@gmail.com>
2024-03-20 19:28:22 +0300

    Там-сям, тяп-ляп

--- ProjEDU/MainForm.cs
+++ ProjEDU/MainForm.cs
@@ -109,8 +109,13 @@ namespace ProjEDU
                 treeView3.Nodes.Add(treeNode);
             }
 
+            using (ContentContext contentContext = new ContentContext())
+            {
+                List<Content> contents = contentContext.Contents.ToList();
+                Content contentFirst = contents.First(c => c.Type == "Test");
+                doc = Content.ToXml(contentFirst.XMLText);
+            }
             ////////////////////////////////////////////////////////////////////////
-            doc.Load("C:\\Users\\Vostro\\source\\repos\\ProjEDU\\ProjEDU\\Test.xml");
             foreach (XmlNode node in doc.SelectNodes("//question"))
             {
                 var treeNode = new TreeNode();
@@ -118,114 +123,14 @@ namespace ProjEDU
                 treeNode.Name = node.Attributes["id"].Value;
                 treeView4.Nodes.Add(treeNode);
             }
-            XmlElement root = doc.DocumentElement;
-            if (root != null)
+            XmlElement test = doc.DocumentElement;
+            if (test != null)
             {
-                panelList = new Panel[doc.SelectNodes("//question").Count+1];
-                foreach (XmlElement node in root)
+                panelList = new Panel[doc.SelectNodes("//question").Count + 1];
+
+                foreach (XmlElement question in test)
                 {
-                    if (treeView4.Nodes.ContainsKey(node.Attributes["id"].Value))
-                    {
-                        int id = int.Parse(node.Attributes["id"].Value);
-                        Panel panelTest = new Panel();
-                        tabPage4.Controls.Add(panelTest);
-                        panelTest.Location = new Point(215, 3);
-                        panelTest.Size = new Size(1046, 379);
-                        panelTest.Name = node.Attributes["id"].Value;
-                        panelTest.Visible = false;
-                        panelList[id] = panelTest;
-                        Label lblQestion = new Label();
-                        lblQestion.Location = new Point(17, 17);
-                        lblQestion.Text = node.Attributes["qtext"].Value;
-                        lblQestion.AutoSize = true;
-                        panelTest.Controls.Add(lblQestion);
-
-                        if (node.Attributes["type"].Value == "1")
-                        {
-                            TableLayoutPanel tableLayoutPanel1 = new TableLayoutPanel();
-                            panelTest.Controls.Add(tableLayoutPanel1);
-                            RadioButton radioButton1 = new RadioButton();
-                            RadioButton radioButton2 = new RadioButton();
-                            RadioButton radioButton3 = new RadioButton();
-                            RadioButton radioButton4 = new RadioButton();
-                            radioButton1.AutoSize = true;
-                            radioButton1.Location = new Point(3, 3);
-                            radioButton1.Size = new Size(117, 24);
-                            radioButton1.TabIndex = 1;
-                            radioButton1.TabStop = true;
-                            radioButton1.Text = "radioButton1";
-                            radioButton1.UseVisualStyleBackColor = true;
-                            radioButton4.AutoSize = true;
-                            radioButton4.Location = new Point(3, 96);
-                            radioButton4.Size = new Size(117, 24);
-                            radioButton4.TabIndex = 4;
-                            radioButton4.TabStop = true;
-                            radioButton4.Text = "radioButton4";
-                            radioButton4.UseVisualStyleBackColor = true;
-                            radioButton2.AutoSize = true;
-                            radioButton2.Location = new Point(3, 34);
-                            radioButton2.Size = new Size(117, 24);
-                            radioButton2.TabIndex = 2;
-                            radioButton2.TabStop = true;
-                            radioButton2.Text = "radioButton2";
-                            radioButton2.UseVisualStyleBackColor = true;
-                            radioButton3.AutoSize = true;
-                            radioButton3.Location = new Point(3, 65);
-                            radioButton3.Size = new Size(117, 24);
-                            radioButton3.TabIndex = 3;
-                            radioButton3.TabStop = true;
-                            radioButton3.Text = "radioButton3";
-                            radioButton3.UseVisualStyleBackColor = true;
-
-                            radioButton1.Name = "1";
-                            radioButton2.Name = "2";
-                            radioButton3.Name = "3";
-                            radioButton4.Name = "4";
-                            tableLayoutPanel1.ColumnCount = 1;
-                            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
-                            tableLayoutPanel1.Controls.Add(radioButton1, 0, 0);
-                            tableLayoutPanel1.Controls.Add(radioButton2, 0, 1);
-                            tableLayoutPanel1.Controls.Add(radioButton3, 0, 2);
-                            tableLayoutPanel1.Controls.Add(radioButton4, 0, 3);
-                            tableLayoutPanel1.Location = new Point(17, 73);
-                            tableLayoutPanel1.Name = "tableLayoutPanel1";
-                            tableLayoutPanel1.RowCount = 4;
-                            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
-                            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
-                            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
-                            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
-                            tableLayoutPanel1.Size = new Size(250, 125);
-                            tableLayoutPanel1.TabIndex = 6;
-                            foreach(XmlNode childnode in node.ChildNodes)
-                            {
-                                foreach(var control in tableLayoutPanel1.Controls)
-                                {
-                                    if(control is RadioButton && 
-                                        ((RadioButton)control).Parent == tableLayoutPanel1 && 
-                                        tableLayoutPanel1.Parent == panelList[id] &&
-                                        childnode.Attributes.Count > 1 &&
-                                        ((RadioButton)control).Name == childnode.Attributes["id"].Value)
-                                    {
-                                        ((RadioButton)control).Text = childnode.Attributes["text"].Value;
-                                    }
-                                }
-                            }
-                        }
-                        if (node.Attributes["type"].Value == "2")
-                        {
-
-                        }
-                        if (node.Attributes["type"].Value == "3")
-                        {
-
-                        }
-                        if (node.Attributes["type"].Value == "4")
-                        {
-
-                        }
-
-                    }
-
+                    LoadTestPanel(question);
                 }
             }
 
@@ -313,41 +218,181 @@ namespace ProjEDU
         private void treeView4_AfterSelect(object sender, TreeViewEventArgs e)
         {
             XmlDocument doc = new XmlDocument();
-            doc.Load("C:\\Users\\Vostro\\source\\repos\\ProjEDU\\ProjEDU\\Test.xml");
-            XmlElement root = doc.DocumentElement;
-            if(root != null)
+            using (ContentContext contentContext = new ContentContext())
             {
-                foreach(XmlElement node in root)
+                List<Content> contents = contentContext.Contents.ToList();
+                Content contentFirst = contents.First(c => c.Type == "Test");
+                doc = Content.ToXml(contentFirst.XMLText);
+            }
+            XmlElement test = doc.DocumentElement;
+            if(test != null)
+            {
+                
+                foreach (XmlElement question in test)
                 {
-                    if (node.Attributes["id"].Value == e.Node.Name)
+                    if (question.Attributes["id"].Value == e.Node.Name)
                     {
-                        panelList[int.Parse(e.Node.Name)].Visible = true;
+                        // Машли? Нормально шли, тимэ
+                        SwitchPanels(panelList[int.Parse(e.Node.Name)]);
+                        // Хотя в целом предложения по оптимизации принимаются
                     }
-                    //if (node.Attributes["type"].Value == "1")
-                    //{
-                    //    panel1.Visible = true;
-                    //    if (node.Attributes["id"].Value == e.Node.Name)
-                    //    {
-                    //        label1.Text = node.Attributes["qtext"].Value;
-                    //        foreach (XmlNode childNode in node.ChildNodes)
-                    //        {
-
-                    //        }
-                    //    }
-                    //}
-                    //if (node.Attributes["id"].Value == e.Node.Name)
-                    //{
-                    //    Panel panel = new Panel();
-                    //    tabPage4.Controls.Add(panel);
-                    //    panel.Location = new Point(215, 3);
-                    //    panel.Size = new Size(1046, 379);
-                    //    panel.Name = e.Node.Name;
-
-
-                    //}
-
                 }
             }
         }
+
+        private void LoadTestPanel(XmlElement question)
+        {
+            if (treeView4.Nodes.ContainsKey(question.Attributes["id"].Value))
+            {
+                int id = int.Parse(question.Attributes["id"].Value);
+                Panel panelTest = new Panel();
+                tabPage4.Controls.Add(panelTest);
+                panelTest.Location = new Point(215, 3);
+                panelTest.Size = new Size(1046, 379);
+                panelTest.Name = question.Attributes["id"].Value;
+                panelTest.Visible = false;
+                panelList[id] = panelTest;
+                Label lblQestion = new Label();
+                lblQestion.Location = new Point(17, 17);
+                lblQestion.Text = question.Attributes["qtext"].Value;
+                lblQestion.AutoSize = true;
+                panelTest.Controls.Add(lblQestion);
+
+                switch (question.Attributes["type"].Value)
+                {
+                    case "1":
+                    {
+                        LoadPanelType1(panelTest, question);
+                    }break;
+                    case "2":
+                    {
+
+                    } break;
+                    case "3":
+                    {
+
+                    } break;
+                    case "4":
+                    {
+
+                    } break;
+                }
+
+                //if (question.Attributes["type"].Value == "1")
+                //{
+                    
+                //}
+                //if (question.Attributes["type"].Value == "2")
+                //{
+
+                //}
+                //if (question.Attributes["type"].Value == "3")
+                //{
+
+                //}
+                //if (question.Attributes["type"].Value == "4")
+                //{
+
+                //}
+
+            }
+        }
+
+        private void SwitchPanels(Panel foundPanel)
+        {
+            foreach (var panel in panelList)
+            {
+                if(panel != null)
+                {
+                    if (foundPanel != panel)
+                    {
+                        panel.Visible = false;
+                        panel.Enabled = false;
+                    }
+                    else
+                    {
+                        panel.Visible = true;
+                        panel.Enabled = true;
+                    }
+                }
+            }
+        }
+
+        private void LoadPanelType1(Panel panelTest, XmlElement question)
+        {
+            TableLayoutPanel tableLayoutPanel1 = new TableLayoutPanel();
+            panelTest.Controls.Add(tableLayoutPanel1);
+            RadioButton radioButton1 = new RadioButton();
+            RadioButton radioButton2 = new RadioButton();
+            RadioButton radioButton3 = new RadioButton();
+            RadioButton radioButton4 = new RadioButton();
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(3, 3);
+            radioButton1.Size = new Size(117, 24);
+            radioButton1.TabIndex = 1;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "radioButton1";
+            radioButton1.UseVisualStyleBackColor = true;
+            radioButton4.AutoSize = true;
+            radioButton4.Location = new Point(3, 96);
+            radioButton4.Size = new Size(117, 24);
+            radioButton4.TabIndex = 4;
+            radioButton4.TabStop = true;
+            radioButton4.Text = "radioButton4";
+            radioButton4.UseVisualStyleBackColor = true;
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(3, 34);
+            radioButton2.Size = new Size(117, 24);
+            radioButton2.TabIndex = 2;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "radioButton2";
+            radioButton2.UseVisualStyleBackColor = true;
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(3, 65);
+            radioButton3.Size = new Size(117, 24);
+            radioButton3.TabIndex = 3;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "radioButton3";
+            radioButton3.UseVisualStyleBackColor = true;
+
+            radioButton1.Name = "1";
+            radioButton2.Name = "2";
+            radioButton3.Name = "3";
+            radioButton4.Name = "4";
+            tableLayoutPanel1.ColumnCount = 1;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.Controls.Add(radioButton1, 0, 0);
+            tableLayoutPanel1.Controls.Add(radioButton2, 0, 1);
+            tableLayoutPanel1.Controls.Add(radioButton3, 0, 2);
+            tableLayoutPanel1.Controls.Add(radioButton4, 0, 3);
+            tableLayoutPanel1.Location = new Point(17, 73);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 4;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.Size = new Size(250, 125);
+            tableLayoutPanel1.TabIndex = 6;
+            foreach (XmlNode childnode in question.ChildNodes)
+            {
+                if (childnode.Name == "ansvar")
+                {
+                    foreach (var control in tableLayoutPanel1.Controls)
+                    {
+                        if (control is RadioButton rButton && rButton.Name == childnode.Attributes["id"].Value)
+                        {
+                            rButton.Text = childnode.Attributes["text"].Value;
+                        }
+                    }
+                }
+            }
+        }
+        private void LoadPanelType2() { } // вот это делай
+        private void LoadPanelType3() { } // сюда иди
+        private void LoadPanelType4() { } // сюда не ходи, башка пиздес будет
+
+        // Осмелюсь предположить, что тут параметры такие же
     }
+    
 }
--- ProjEDU/Model/ContentContext.cs
+++ ProjEDU/Model/ContentContext.cs
@@ -4,8 +4,9 @@ namespace ProjEDU.Model
 {
     public class ContentContext : DbContext
     {
-        //public ContentContext() : base("Data Source=HOME-PC;Initial Catalog=ProjEDU;Persist Security Info = True;Integrated Security=True")
-        public ContentContext(): base("Data Source=(LocalDb)\\LocalDB;Initial Catalog=ProjEDU;Persist Security Info = True;Integrated Security=True")
+        public ContentContext() : base("Data Source=HOME-PC;Initial Catalog=ProjEDU;Persist Security Info = True;Integrated Security=True")
+        // public UserContext() : base("Data Source=(LocalDb)\\LocalDB;Initial Catalog=ProjEDU;Persist Security Info = True;Integrated Security=True")
+
         { }
         public DbSet<Content> Contents { get; set; }
 
